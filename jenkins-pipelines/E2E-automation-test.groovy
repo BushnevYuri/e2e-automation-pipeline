0a182b56b32b755fbe5a3154bee991e8c5edbb43
Yuri Bushnev <ybushnev@alpha-sense.com>
2017-10-08 21:33:33 +0300

    Jenkinsfile for E2E automation test

---
+++ jenkins-pipelines/E2E-automation-test.groovy
@@ -0,0 +1,47 @@
+node {
+    stage('Git checkout') { // for display purposes
+        git 'https://github.com/BushnevYuri/e2e-automation-pipeline.git'
+    }
+    stage('Smoke') {
+        try {
+            sh "mvn clean verify -Dtags='type:Smoke'"
+        } catch (err) {
+
+        } finally {
+            publishHTML (target: [
+                    reportDir: 'target/site/serenity',
+                    reportFiles: 'index.html',
+                    reportName: "Smoke tests report"
+            ])
+        }
+    }
+    stage('API') {
+        try {
+            sh "mvn clean verify -Dtags='type:API'"
+        } catch (err) {
+
+        } finally {
+            publishHTML (target: [
+                    reportDir: 'target/site/serenity',
+                    reportFiles: 'index.html',
+                    reportName: "API tests report"
+            ])
+        }
+    }
+    stage('UI') {
+        try {
+            sh "mvn clean verify -Dtags='type:UI'"
+        } catch (err) {
+
+        } finally {
+            publishHTML (target: [
+                    reportDir: 'target/site/serenity',
+                    reportFiles: 'index.html',
+                    reportName: "UI tests report"
+            ])
+        }
+    }
+    stage('Results') {
+        junit '**/target/failsafe-reports/*.xml'
+    }
+}
