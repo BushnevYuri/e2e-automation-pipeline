9d3c2548e7a5046714e5d88c29c647923a2badbf
sag-c8y <73693771+sag-c8y@users.noreply.github.com>
2020-11-26 18:23:23 +0530

    Update InstallChrome.sh

--- InstallChrome.sh
+++ InstallChrome.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -ex
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt update && sudo apt-get update
 sudo apt install ./google-chrome-stable_current_amd64.deb -y
